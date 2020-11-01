16e9c52bd4e7fe415f54817168aa19aea8306b5d
HorKrisz <horvathkrisz00@gmail.com>
2020-11-01 13:19:55 +0100

    Created Project folder and added basic webpage

---
+++ Project/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nomad's Webshop</title>
+</head>
+<body>
+    <div id="page">
+        <header>Nomad's Webshop</header>
+        <nav></nav>
+        <content></content>
+        <footer>Copyright &copy; 2020 Nomad Team</footer>
+    </div>
+</body>
+</html>
