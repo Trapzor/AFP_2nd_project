012447478ae915e35ee1fd0f6f0edbe6b7de5a2c
PHorvath1 <hpeti291@gmail.com>
2020-11-13 18:16:27 +0100

    Added product listing to Browse.php

--- Project/backend/products/browse.php
+++ Project/backend/products/browse.php
@@ -1,1 +1,19 @@
-Browse
+<?php
+$query = "SELECT id, name, image, price FROM products";
+require_once 'backend/dbFunctions.php';
+$list = getList($query);
+?>
+<div class="productsHeader"> <h2>Termékeink</h2> </div>
+
+<div class="productList">
+    <?php foreach ($list as $i): ?>
+        <div class="productCard">
+            <h4><?=$i['name']?></h4>
+            <img src="Product_Images/<?=$i['image']?>">
+            <div class="orderContainer">
+                <?=$i['price']?> Ft
+                <button>Kosárba</button>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
