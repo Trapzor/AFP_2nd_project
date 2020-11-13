1bb9bc73f9d585cccef5bcfe1e849c90ed48c255
PHorvath1 <hpeti291@gmail.com>
2020-11-13 15:24:28 +0100

    Added getRecord and executeDML functions to dbFunctions

--- Project/backend/dbFunctions.php
+++ Project/backend/dbFunctions.php
@@ -6,4 +6,23 @@ function connectToDatabase(){
     return $connection;
 }
 
+function getRecord($queryString, $queryParams = []){
+    $connection = connectToDatabase();
+    $statement = $connection->prepare($queryString);
+    $success = $statement->execute($queryParams);
+    $result = $success ? $statement->fetch(PDO::FETCH_ASSOC) : [];
+    $statement->closeCursor();
+    $connection = null;
+    return $result;
+}
+
+function executeDML($queryString, $queryParams = []){
+    $connection = connectToDatabase();
+    $statement = $connection->prepare($queryString);
+    $success = $statement->execute($queryParams);
+    $statement->closeCursor();
+    $connection = null;
+    return $success;
+}
+
 ?>
