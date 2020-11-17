ec94e358de4a94b08c16977b5691e27349a73c34
HorKrisz <horvathkrisz00@gmail.com>
2020-11-17 11:17:29 +0100

    Added icons for nav buttons

--- Project/backend/nav.php
+++ Project/backend/nav.php
@@ -4,14 +4,14 @@ if (empty($_GET['p']))
 ?>
 
 <div class="navbutton <?=$_GET['p']=='home'?"active":""?>">
-    <a href="index.php?p=home">Főoldal</a>
+    <a href="index.php?p=home"><i class="fa fa-home"></i> Főoldal</a>
 </div>
 <div class="navbutton <?=$_GET['p']=='browse'?"active":""?>">
-    <a href="index.php?p=browse">Termékeink</a>
+    <a href="index.php?p=browse"><i class="fa fa-bars"></i> Termékeink</a>
 </div>
 <div class="navbutton <?=$_GET['p']=='favorites'?"active":""?>">
-    <a href="index.php?p=favorites">Kedvencek</a>
+    <a href="index.php?p=favorites"><i class="fa fa-heart"></i> Kedvencek</a>
 </div>
 <div class="navbutton <?=$_GET['p']=='cart'?"active":""?>">
-    <a href="index.php?p=cart">Kosár</a>
+    <a href="index.php?p=cart"><i class="fa fa-shopping-cart"></i> Kosár</a>
 </div>
