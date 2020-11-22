c8998620a1f9c7fb74d01ab5a47bec5f996fd2e8
HorKrisz <horvathkrisz00@gmail.com>
2020-11-22 12:26:41 +0100

    Added checkings to the login page

--- Project/backend/user/login.php
+++ Project/backend/user/login.php
@@ -1,3 +1,20 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['loginSubmit'])) {
+  $postData = [
+    'email' => $_POST['userEmail'],
+    'password' => $_POST['userPassword']
+  ];
+
+  if (empty($postData['email']) || empty($postData['password'])) {
+    echo "Hiányzó adat(ok)!";
+  } else if (!UserLogin($postData['email'], $postData['password'])) {
+    echo "Hibás felhasználónév vagy jelszó!";
+  }
+
+  $postData['password'] = "";
+}
+?>
+
 <form class="login_form" method="post">
     <div class="form_login_row">
         <label for="userEmail">Email cím</label>
@@ -5,7 +22,7 @@
     </div>
     <div class="form_login_row">
         <label for="userPassword">Jelszó</label>
-        <input type="password" name="userPassword" id="userPassword" placeholder="Jelszó">
+        <input type="password" name="userPassword" id="userPassword" placeholder="Jelszó" value="">
     </div>
     <div class="form_login_row">
         <input class="btnSubmit" type="submit" name="loginSubmit" value="Bejelentkezés">
