8a10594aa954a6204030f7c9fcf7c16a17ae5f49
cserneczkybalint <cserneczkybalint@gmail.com>
2020-11-23 15:13:00 +0100

    Created a logo, changed the "nomad's webshop" text in header to the logo

--- Project/backend/header.php
+++ Project/backend/header.php
@@ -1,4 +1,4 @@
-<h3>Nomad's Webshop</h3>
+<img src="frontend/nomads_logo.png" alt="our logo" class="nomads_logo">
 <div class="userInfo">
     <?php if (!IsUserLoggedIn()): ?>
         <i class="fa fa-user-circle-o"></i> <a href="index.php?p=login">Bejelentkezés</a>
