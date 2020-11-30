aa14afc0109cf413faa0ff5bc529a4fe46d90654
HorKrisz <horvathkrisz00@gmail.com>
2020-11-30 20:23:31 +0100

    Changed the style of cart

--- Project/backend/cart.php
+++ Project/backend/cart.php
@@ -18,11 +18,22 @@ $shopping_list = getList($query, $params);
 ?>
 <div class="cart">
 <h2>Kosár</h2>
-    <?php if (Count($shopping_list) > 0) :?>
-        <?php foreach ($shopping_list as $l) : ?>
-            <div class="item">
-                <span><?=$l['name']?><span> <span id='itemprice'><?=$l['price']?> Ft</span>
-            </div>
-        <?php endforeach; ?>
-    <?php endif; ?>
+    <div class="cart-outline">
+        <span class="cart-header cart-item-name">Megnevezés</span>
+        <span class="cart-header cart-item-quantity">Darabszám</span>
+        <span class="cart-header cart-item-price">Ár</span>
+        <?php if (Count($shopping_list) > 0) :?>
+            <?php foreach ($shopping_list as $l) : ?>
+                <div class="item">
+                    <span><?=$l['name']?></span> 
+                    <div class="cart-quantity-panel">
+                        <a href="#" class="cart-quantity-button"><i class="fa fa-minus"></i></a>
+                        <span class="cart-quantity">1</span>
+                        <a href="#" class="cart-quantity-button"><i class="fa fa-plus"></i></a>
+                    </div>
+                    <span class='itemprice'><?=$l['price']?> Ft</span>
+                </div>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </div>
 </div>
