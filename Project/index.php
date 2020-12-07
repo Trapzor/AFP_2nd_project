1f1878e77220620afd94d4e74016e2b3541ca07e
cserneczkybalint <cserneczkybalint@gmail.com>
2020-12-07 23:35:13 +0100

    Added cookie warning script

--- Project/index.php
+++ Project/index.php
@@ -9,6 +9,8 @@ require_once 'backend/userManager.php';
     <title>Nomad's Webshop</title>
     <link rel="stylesheet" href="frontend/style.css " type='text/css'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="frontend/purecookie.css" async />
+    <script src="frontend/purecookie.js" async></script>
     <script src="frontend/script.js"></script>
 </head>
 <body>
