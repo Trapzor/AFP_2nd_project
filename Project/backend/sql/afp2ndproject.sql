c409836bd27cda04681d9db97b6ebca898efe8b5
Erdélyi Roland <erdelyi.roland99@icloud.com>
2020-12-08 09:47:23 +0100

    Added a proper .sql file

--- Project/backend/sql/afp2ndproject.sql
+++ Project/backend/sql/afp2ndproject.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2020. Dec 01. 10:19
+-- Létrehozás ideje: 2020. Dec 08. 09:46
 -- Kiszolgáló verziója: 10.4.11-MariaDB
--- PHP verzió: 7.4.3
+-- PHP verzió: 7.4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -31,8 +30,8 @@ SET time_zone = "+00:00";
 CREATE TABLE `bugreports` (
   `id` int(11) NOT NULL,
   `uid` int(11) NOT NULL,
-  `report` varchar(500) NOT NULL,
-  `date` date NOT NULL
+  `report` varchar(250) NOT NULL,
+  `date` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -75,11 +74,32 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `image`, `category`, `rating`, `quantity`, `price`) VALUES
-(1, 'IPhone X', 'IPhone_X.jpg', 'okostelefon', 5, 10, 279990),
+(1, 'IPhone X', 'IPhone_X.jpg', 'okostelefon', 4, 10, 279990),
 (2, 'JBL Live300twsblu', 'JBL_Live300twsblu.jpg', 'fülhallgató', 5, 10, 24999),
-(3, 'Lenovo V15', 'Lenovo_V15.jpg', 'laptop', 4, 21, 114990),
+(3, 'Lenovo V15', 'Lenovo_V15.jpg', 'laptop', 1, 21, 114990),
 (4, 'Samsung UE43TU8502', 'Samsung_UE43TU8502.jpg', 'tv', 5, 10, 124990),
 (5, 'Serioux HT5100C', 'Serioux_HT5100C.jpg', 'hangszóró', 4, 12, 24990);
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `rating`
+--
+
+CREATE TABLE `rating` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `rating_value` varchar(1) NOT NULL,
+  `rating_description` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- A tábla adatainak kiíratása `rating`
+--
+
+INSERT INTO `rating` (`id`, `product_id`, `user_id`, `rating_value`, `rating_description`) VALUES
+(17, 2, 3, '5', 'awd');
 
 -- --------------------------------------------------------
 
@@ -100,7 +120,9 @@ CREATE TABLE `shopping_cart` (
 INSERT INTO `shopping_cart` (`user_id`, `product_id`, `quantity`) VALUES
 (1, 1, 1),
 (1, 1, 1),
-(1, 2, 1);
+(1, 2, 1),
+(2, 1, 1),
+(2, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -117,6 +139,13 @@ CREATE TABLE `users` (
   `password` varchar(250) NOT NULL,
   `permission` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- A tábla adatainak kiíratása `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `address`, `phone_number`, `email`, `password`, `permission`) VALUES
+(2, 'Roland', 'awdawdawdawd', '06201111111', 'Teszt@elek.com', 'c8499454bada15f6d76bbf8cf133960f93f9b4eb', 0);
 
 --
 -- Indexek a kiírt táblákhoz
@@ -141,6 +170,12 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
+-- A tábla indexei `rating`
+--
+ALTER TABLE `rating`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- A tábla indexei `users`
 --
 ALTER TABLE `users`
@@ -155,7 +190,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT a táblához `bugreports`
 --
 ALTER TABLE `bugreports`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT a táblához `products`
@@ -164,10 +199,16 @@ ALTER TABLE `products`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT a táblához `rating`
+--
+ALTER TABLE `rating`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
 -- AUTO_INCREMENT a táblához `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
