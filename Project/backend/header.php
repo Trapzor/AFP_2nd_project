0327aa00f68331995d7d27750f435ae335281591
cserneczkybalint <cserneczkybalint@gmail.com>
2020-12-08 04:38:21 +0100

    Aligned username to its container

--- Project/backend/header.php
+++ Project/backend/header.php
@@ -9,7 +9,7 @@
     </a>
     <?php else: ?>
         <div class="user_panel">
-            <i class="fa fa-user-circle-o"></i> <?=$_SESSION['name']; ?>
+            <div class="alignUsername"><i class="fa fa-user-circle-o"></i> <?=$_SESSION['name']; ?></div>
             <hr/>
             <div class="modify_button"><a href="index.php?p=profile"><i class="fa fa-gear"></i> Fiókadatok módosítása</a></div>
             <div class="logout_button"><a href="index.php?p=logout"><i class="fa fa-power-off"></i> Kijelentkezés</a></div>
