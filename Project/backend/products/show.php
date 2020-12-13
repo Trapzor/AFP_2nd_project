dfb860717f96b6696b9c681eda1ca651f8fbd1c2
PHorvath1 <hpeti291@gmail.com>
2020-12-13 17:07:13 +0100

    Added product removing function in the show products page
    Only accessible if permission level is at least 1

--- Project/backend/products/show.php
+++ Project/backend/products/show.php
@@ -28,23 +28,42 @@ if(array_key_exists('f',$_GET) && !empty($_GET['f'])) {
             echo("A funkció használatához először jelentkezzen be");
     }
 }
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['removeProduct']) && IsUserLoggedIn() && $_SESSION['permission'] > 0) {
+    $query = "DELETE FROM products WHERE id=:id";
+    $params = [
+        ':id' => $_GET['s']
+    ];
+    if (executeDML($query, $params)) {
+        header('Location: ./index.php?p=browse');
+    } else {
+        echo("<script>alert('Hiba a törlés közben');</script>");
+    }
+}
 ?>
 
 
 <div class="product">
     <a href="index.php?p=browse" id="backToBrowse"><i class="fa fa-arrow-left"></i></a>
+    <?php if(IsUserLoggedIn() && $_SESSION['permission'] > 0) : ?>
+        <form method="post">
+            <button name="removeProduct" class="removeButton"><i class="fa fa-trash"></i></button>
+        </form>
+    <?php endif; ?>
     <a href="index.php?p=show&s=<?=$product['id']?>&f=<?=$product['id']?>" id="addToFavorite"><i class="fa fa-heart"></i></a>
 
     <h3><?=$product['name']?></h3>
     <hr>
     <img src="Product_images/<?=$product['image']?>">
+    <h4 id="ratinglabel">Értékelés:</h4>
     <div id="rating"></div>
     <a href="index.php?p=rate&s=<?=$product['id']?>" id="rateProduct"></i>Értékelés</a>
     <a href="index.php?p=listrate&s=<?=$product['id']?>" id="rateProduct"></i>Értékelések megtekintése</a>
+    <h4 id="categorylabel">Kategória:</h4>
     <h4 id="category"><?=$product['category']?></h4>
     <h3><?=$product['price']?> Ft</h3>
     <hr>
-    <button>
+    <button class="add-to-cart-button">
         <a  href="index.php?p=cart&i=<?=$product['id']?>">
             <i class="fa fa-shopping-basket"></i>
         </a>
