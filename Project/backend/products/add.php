05562d7ce54198da216a087b31008604597efe7b
HorKrisz <horvathkrisz00@gmail.com>
2020-12-13 11:29:17 +0100

    Made image uploadable from anywhere on the add page

--- Project/backend/products/add.php
+++ Project/backend/products/add.php
@@ -2,7 +2,7 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add'])) {
     $postData = [
         'pname' => $_POST['pname'],
-        'pimage' => $_POST['pimage'],
+        'pimage' => $_FILES['pimage']['name'],
         'category' => $_POST['category'],
         'quantity' => $_POST['quantity'],
         'price' => $_POST['price']
@@ -11,6 +11,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add'])) {
     if (empty($postData['pname']) || empty($postData['pimage']) || empty($postData['category']) || empty($postData['quantity']) || empty($postData['price'])) {
         echo "Hiányzó adatok!";
     } else {
+        $target_dir = '/AFP_2nd_project/Project/Product_Images/';
+        $target_file = $_SERVER['DOCUMENT_ROOT'].$target_dir.basename($_FILES['pimage']['name']);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+            echo "Csak jpg, png és jpeg formátum használható!";
+            $uploadOk = 0;
+        }
+
+        if ($uploadOk == 0) {
+            echo "Fájl feltöltése sikertelen.";
+        } else {
+            move_uploaded_file($_FILES['pimage']['tmp_name'], $target_file);
+        }
+
         $query = "INSERT INTO products (name, image, category, rating, quantity, price) VALUES (:name, :image, :category, :rating, :quantity, :price)";
         $params = [
             ':name' => $postData['pname'],
@@ -32,14 +48,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add'])) {
     <img id="preview" alt="A feltöltött kép">
 </div>
 <div class="addFormContainer">
-    <form method="post">
+    <form method="post" enctype="multipart/form-data">
         <div class="form_add_row">
             <label for="pname"><b>Név</b></label>
             <input type="text" name="pname" placeholder="Név" class="addElements">
         </div>
         <div class="form_add_row">
             <label for="pimage"><b>Kép</b></label>
-            <small>(A képnek a Product_Images mappában kell lennie!)</small>
             <input type="file" name="pimage" class="addElements" accept="image/*" onchange="loadFile(event)">
         </div>
         <div class="form_add_row">
